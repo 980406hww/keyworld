c9771138f13b38a53da5bf4c06c76441f20150b7
Duchengfu <duchengfu@163.com>
2017-08-17 14:53:26 +0800

    Revise Customer

--- src/main/java/com/keymanager/monitoring/service/CustomerService.java
+++ src/main/java/com/keymanager/monitoring/service/CustomerService.java
@@ -1,10 +1,13 @@
 package com.keymanager.monitoring.service;
 
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.mapper.Wrapper;
 import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.keymanager.monitoring.criteria.CustomerCriteria;
 import com.keymanager.monitoring.dao.CustomerDao;
 import com.keymanager.monitoring.entity.Customer;
+import com.keymanager.monitoring.entity.CustomerKeyword;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,5 +45,37 @@ public class CustomerService extends ServiceImpl<CustomerDao, Customer> {
 	public Page<Customer> searchCustomers(Page<Customer> page, CustomerCriteria customerCriteria){
 		page.setRecords(customerDao.searchCustomers(page, customerCriteria));
 		return page;
+	}
+
+	public void updateCustomer(Customer customer) {
+		Customer oldCustomer = customerDao.selectById(customer.getUuid());
+		if (oldCustomer != null) {
+			oldCustomer.setContactPerson(customer.getContactPerson());
+			oldCustomer.setQq(customer.getQq());
+			oldCustomer.setEmail(customer.getEmail());
+			oldCustomer.setTelphone(customer.getTelphone());
+			oldCustomer.setAlipay(customer.getAlipay());
+			oldCustomer.setPaidFee(customer.getPaidFee());
+			oldCustomer.setRemark(customer.getRemark());
+			oldCustomer.setType(customer.getType());
+			oldCustomer.setStatus(customer.getStatus());
+			oldCustomer.setUserID(customer.getUserID());
+			oldCustomer.setEntryType(customer.getEntryType());
+			oldCustomer.setUpdateTime(new Date());
+			customerDao.updateById(oldCustomer);
+		}
+	}
+
+	public int addCustomer(Customer customer){
+		customerDao.insert(customer);
+		return customerDao.selectLastId();
+	}
+
+	public void deleteCustomer(long uuid){
+		customerDao.deleteById(uuid);
+		CustomerKeyword customerKeyword = new CustomerKeyword();
+		customerKeyword.setCustomerUuid(uuid);
+		Wrapper wrapper = new EntityWrapper(customerKeyword);
+		customerKeywordService.delete(wrapper);
 	}
 }
