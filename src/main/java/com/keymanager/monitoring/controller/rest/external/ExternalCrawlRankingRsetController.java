184bd61a12938ae202fd02d1471ba42b248bf4ca
qiuzongwang <1477552894@qq.com>
2017-09-28 16:16:46 +0800

    edit clsaa

--- src/main/java/com/keymanager/monitoring/controller/rest/external/ExternalCrawlRankingRsetController.java
+++ src/main/java/com/keymanager/monitoring/controller/rest/external/ExternalCrawlRankingRsetController.java
@@ -1,6 +1,7 @@
 package com.keymanager.monitoring.controller.rest.external;
 
 import com.keymanager.monitoring.controller.SpringMVCBaseController;
+import com.keymanager.monitoring.criteria.BaseCriteria;
 import com.keymanager.monitoring.criteria.CaptureRankCriteria;
 import com.keymanager.monitoring.entity.CaptureCurrentRankJob;
 import com.keymanager.monitoring.entity.CustomerKeyword;
@@ -16,6 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +28,7 @@ import java.util.Map;
 /**
  * Created by shunshikj24 on 2017/9/27.
  */
+@RestController
 @RequestMapping(value = "/external/crawlRanking")
 public class ExternalCrawlRankingRsetController extends SpringMVCBaseController {
     private static Logger logger = LoggerFactory.getLogger(ExternalCrawlRankingRsetController.class);
@@ -36,16 +39,20 @@ public class ExternalCrawlRankingRsetController extends SpringMVCBaseController 
     @Autowired
     private CustomerKeywordService customerKeywordService;
 
-    @RequestMapping(value="/crawlRankingExternalInterface",method = RequestMethod.GET)
-    public ResponseEntity<?> doCrawlRanking() {
+    @RequestMapping(value = "/crawlRankingExternalInterface", method = {RequestMethod.GET, RequestMethod.POST})
+    public ResponseEntity<?> doCrawlRanking(@RequestBody BaseCriteria baseCriteria) {
+        String userName = baseCriteria.getUserName();
+        String password = baseCriteria.getUserName();
         try {
+            if (validUser(userName, password)) {
                 CaptureCurrentRankJob captureCurrentRankJobs = captureCurrentRankJobService.provideCaptureCurrentRankJob();
                 return new ResponseEntity<Object>(captureCurrentRankJobs, HttpStatus.OK);
-
-        }catch(Exception ex) {
-            logger.error(ex.getMessage());
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
             return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
         }
+        return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
     }
 
     @RequestMapping(value = "/getCustomerKeywordForCapturePosition", method = RequestMethod.POST)
@@ -55,13 +62,13 @@ public class ExternalCrawlRankingRsetController extends SpringMVCBaseController 
         String terminalType = TerminalTypeMapping.getTerminalType(request);
         try {
             if (validUser(userName, password)) {
-                CaptureCurrentRankJob captureCurrentRankJob=captureRankCriteria.getCaptureRankJob();
+                CaptureCurrentRankJob captureCurrentRankJob = captureRankCriteria.getCaptureRankJob();
                 String[] groupNames = captureCurrentRankJob.getGroupNames().split(",");
-                Long customerID=captureCurrentRankJob.getCustomerID();
+                Long customerID = captureCurrentRankJob.getCustomerID();
                 CustomerKeyword customerKeyword = customerKeywordService.searchTitleAndUrl(groupNames, customerID);
-                return new ResponseEntity<Object>(customerKeyword,HttpStatus.OK);
+                return new ResponseEntity<Object>(customerKeyword, HttpStatus.OK);
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             logger.error(ex.getMessage());
             return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
         }
