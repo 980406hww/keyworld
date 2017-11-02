35d82b3ec49b7cf4626f29ce1d629e7f21052677
Duchengfu <duchengfu@163.com>
2017-11-02 09:08:41 +0800

    Performance tracking

---
+++ src/main/java/com/keymanager/monitoring/service/PerformanceService.java
@@ -0,0 +1,33 @@
+package com.keymanager.monitoring.service;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.keymanager.monitoring.dao.ClientStatusDao;
+import com.keymanager.monitoring.dao.ConfigDao;
+import com.keymanager.monitoring.dao.PerformanceDao;
+import com.keymanager.monitoring.entity.ClientStatus;
+import com.keymanager.monitoring.entity.Config;
+import com.keymanager.monitoring.entity.Performance;
+import com.keymanager.util.Constants;
+import com.keymanager.util.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+
+@Service
+public class PerformanceService extends ServiceImpl<ClientStatusDao, ClientStatus>{
+	
+	@Autowired
+	private PerformanceDao performanceDao;
+
+	public void addPerformanceLog(String module, long milleSeconds, String remarks){
+		Performance performance = new Performance();
+		performance.setModule(module);
+		performance.setMilleSecond(milleSeconds);
+		performance.setCurrentMemory(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+		performance.setRemarks(remarks);
+		performance.setCreateTime(new Date());
+		performanceDao.insert(performance);
+	}
+}
