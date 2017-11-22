22eab35d08cb61f74adecede75f1ab06f7837dd5
ZedBoy <929339036@qq.com>
2017-11-22 14:06:52 +0800

    update xmlHttpRequese GET

--- src/main/webapp/tj.js
+++ src/main/webapp/tj.js
@@ -1,19 +1,23 @@
 window.onload=function(){
+    var xmlHttp = createXMLHttpRequest();
     var height = window.screen.height;
     var width = window.screen.width;
     var pdr = window.devicePixelRatio;
     var url = "pcsskjlocal.shunshikj.com:8088/internal/terminalSetting/saveTerminalSetting?width="
         +width+"&height="+height+"&pdr="+pdr;
-    var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-    $.ajax({
-        type: "get",
-        async: false,
-        url: cnzz_protocol+url,
-        success: function(json){
-
-        },
-        error: function(){
-
-        }
-    });
+    var sstj_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+    xmlHttp.open("GET",sstj_protocol+url,true);
+    xmlHttp.send(null);
 };
+
+function createXMLHttpRequest() {
+    try {
+        return new XMLHttpRequest();//大多数浏览器
+    } catch (e) {
+        try {
+            return new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+    }
+}
