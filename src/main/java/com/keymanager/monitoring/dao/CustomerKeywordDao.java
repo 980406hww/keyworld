e35795d4d808c02febcea3a91e455e42916ce151
Duchengfu <duchengfu@163.com>
2018-01-20 15:12:00 +0800

    Fix capture index issue

--- src/main/java/com/keymanager/monitoring/dao/CustomerKeywordDao.java
+++ src/main/java/com/keymanager/monitoring/dao/CustomerKeywordDao.java
@@ -125,6 +125,4 @@ CustomerKeywordDao extends BaseMapper<CustomerKeyword> {
     List<String> searchCustomerKeywordSummaryInfo(@Param("entryType")String entryType, @Param("customerUuid")Long customerUuid);
 
     String[] searchCustomerNegativeKeywords(@Param("customerUuid")long customerUuid);
-
-    void updateCustomerKeywordIndex(@Param("customerKeyword") CustomerKeyword customerKeyword);
 }
