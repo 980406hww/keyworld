f57c8d472d3d80df0ccbc53402e215b2ca748f1e
myCoding26 <gz26@foxmail.com>
2018-06-13 15:06:26 +0800

    add findInitialNegativeRanks

--- src/main/java/com/keymanager/monitoring/service/NegativeRankService.java
+++ src/main/java/com/keymanager/monitoring/service/NegativeRankService.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.service.impl.ServiceImpl;
 import com.keymanager.monitoring.criteria.NegativeRankCriteria;
 import com.keymanager.monitoring.dao.NegativeRankDao;
 import com.keymanager.monitoring.entity.NegativeRank;
+import com.keymanager.util.Utils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.time.DateUtils;
 import org.slf4j.Logger;
@@ -13,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class NegativeRankService extends ServiceImpl<NegativeRankDao, NegativeRank> {
@@ -43,5 +46,15 @@ public class NegativeRankService extends ServiceImpl<NegativeRankDao, NegativeRa
     public Page<NegativeRank> searchNegativeRanks(Page page,NegativeRankCriteria negativeRankCriteria){
         page.setRecords(negativeRankDao.searchNegativeRanks(page,negativeRankCriteria));
         return page;
+    }
+
+    public Map<String, Object> findInitialNegativeRanks(NegativeRankCriteria negativeRankCriteria) {
+        negativeRankCriteria.setSearchDate(DateUtils.setHours(negativeRankCriteria.getSearchDate(), 0));
+        List<NegativeRank> negativeRanks = negativeRankDao.findInitialNegativeRanks(negativeRankCriteria.getSearchEngine(), negativeRankCriteria.getSearchDate());
+        Map<String, Object> initialNegativeRankMap = new HashMap<String, Object>();
+        for (NegativeRank negativeRank : negativeRanks) {
+            initialNegativeRankMap.put(negativeRank.getKeyword(), negativeRank);
+        }
+        return initialNegativeRankMap;
     }
 }
