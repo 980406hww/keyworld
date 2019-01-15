a08de27b89a3a125fee6076e7faa6fff353a5bf0
duchengfu <duchengfu@163.com>
2019-01-15 11:39:28 +0800

    capture Index logic revising

--- src/main/java/com/keymanager/monitoring/service/PerformanceService.java
+++ src/main/java/com/keymanager/monitoring/service/PerformanceService.java
@@ -22,12 +22,14 @@ public class PerformanceService extends ServiceImpl<ClientStatusDao, ClientStatu
 	private PerformanceDao performanceDao;
 
 	public void addPerformanceLog(String module, long milleSeconds, String remarks){
-//		Performance performance = new Performance();
-//		performance.setModule(module);
-//		performance.setMilleSecond(milleSeconds);
-//		performance.setCurrentMemory(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
-//		performance.setRemarks(remarks);
-//		performance.setCreateTime(new Date());
-//		performanceDao.insert(performance);
+		if("ResetInfoDailySchedule".equals(module)) {
+			Performance performance = new Performance();
+			performance.setModule(module);
+			performance.setMilleSecond(milleSeconds);
+			performance.setCurrentMemory(Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
+			performance.setRemarks(remarks);
+			performance.setCreateTime(new Date());
+			performanceDao.insert(performance);
+		}
 	}
 }
--- src/main/java/com/keymanager/monitoring/shedule/ResetInfoDailySchedule.java
+++ src/main/java/com/keymanager/monitoring/shedule/ResetInfoDailySchedule.java
@@ -1,5 +1,6 @@
 package com.keymanager.monitoring.shedule;
 
+import com.keymanager.monitoring.entity.ClientCookie;
 import com.keymanager.monitoring.entity.CustomerKeyword;
 import com.keymanager.monitoring.service.*;
 import org.slf4j.Logger;
@@ -23,14 +24,21 @@ public class ResetInfoDailySchedule {
 	@Autowired
 	private ClientStatusService clientStatusService;
 
+	@Autowired
+	private PerformanceService performanceService;
 
 	public void runTask(){
 		logger.info("============= "+" Reset informaiton Daily Task "+"===================");
 		try {
+			long startMilleSeconds = System.currentTimeMillis();
+			performanceService.addPerformanceLog("ResetInfoDailySchedule", System.currentTimeMillis() - startMilleSeconds, "starting");
+
 			customerKeywordInvalidCountLogService.addCustomerKeywordInvalidCountLog();
 			configService.updateOptimizationDateAsToday();
 			customerKeywordService.resetOptimizationInfo();
 			clientStatusService.resetOptimizationInfo();
+
+			performanceService.addPerformanceLog("ResetInfoDailySchedule", System.currentTimeMillis() - startMilleSeconds, "ended");
 			logger.info("============= "+" End Reset informaiton Daily Task "+"===================");
 		} catch (Exception e) {
 			logger.error("Client Upgrade is error" + e.getMessage());
