aae248194af5cb1390d304aa73e831b2c225f58d
steven.zhou <3047409257@qq.com>
2019-03-27 09:45:23 +0800

    fix CustomerKeywordRestController null issue

--- src/main/java/com/keymanager/monitoring/service/CustomerKeywordService.java
+++ src/main/java/com/keymanager/monitoring/service/CustomerKeywordService.java
@@ -102,7 +102,9 @@ public class CustomerKeywordService extends ServiceImpl<CustomerKeywordDao, Cust
     private CustomerExcludeKeywordService customerExcludeKeywordService;
 
     public Page<CustomerKeyword> searchCustomerKeywords(Page<CustomerKeyword> page, CustomerKeywordCriteria customerKeywordCriteria){
-        customerKeywordCriteria.setUrl(customerKeywordCriteria.getUrl().replace("https://", "").replace("http://", "").replace("www.", ""));
+        if (null != customerKeywordCriteria.getUrl()) {
+            customerKeywordCriteria.setUrl(customerKeywordCriteria.getUrl().replace("https://", "").replace("http://", "").replace("www.", ""));
+        }
         page.setRecords(customerKeywordDao.searchCustomerKeywordsPageForCustomer(page, customerKeywordCriteria));
         return page;
     }
