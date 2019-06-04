47fb4c54c363ecae9b2182a9f014cef05a108324
steven.zhou <3047409257@qq.com>
2019-06-04 17:03:01 +0800

    将同时间的定时任务合并为一个，解决定时任务不执行的问题

--- src/main/java/com/keymanager/monitoring/shedule/TerminalRefreshStatInfoSchedule.java
+++ src/main/java/com/keymanager/monitoring/shedule/TerminalRefreshStatInfoSchedule.java
@@ -1,6 +1,10 @@
 package com.keymanager.monitoring.shedule;
 
+import com.keymanager.monitoring.entity.Config;
+import com.keymanager.monitoring.service.ConfigService;
 import com.keymanager.monitoring.service.CustomerKeywordRefreshStatInfoService;
+import com.keymanager.monitoring.service.CustomerKeywordService;
+import com.keymanager.util.Constants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,12 +21,35 @@ public class TerminalRefreshStatInfoSchedule {
     @Autowired
     private CustomerKeywordRefreshStatInfoService customerKeywordRefreshStatInfoService;
 
+    @Autowired
+    private CustomerKeywordService customerKeywordService;
+
+    @Autowired
+    private ConfigService configService;
+
     public void runTask(){
         logger.info("============= " + " Terminal Refresh Stat Info Task "+"===================");
         try{
             customerKeywordRefreshStatInfoService.updateCustomerKeywordStatInfo();
         } catch (Exception e) {
             logger.error("Terminal Refresh Stat Info is error" + e.getMessage());
+        }
+
+        logger.info("============= "+" Change Optimize Group Name Task "+"===================");
+        try {
+            customerKeywordService.changeOptimizeGroupName();
+        } catch (Exception e) {
+            logger.error(" Change Optimize Group Name is error" + e.getMessage());
+        }
+
+        logger.info("============= "+" Change No Entered Keyword Group Name Task  "+"===================");
+        try {
+            Config config = configService.getConfig(Constants.CONFIG_TYPE_NOENTEREDKEYWORDSCHEDULE_SWITCH, Constants.CONFIG_KEY_SWITCHNUMBER);
+            if (config != null && config.getValue().equals("1")) {
+                customerKeywordService.updateNoEnteredKeywordGroupName();
+            }
+        } catch (Exception e) {
+            logger.error(" Change No Entered Keyword Group Name is error" + e.getMessage());
         }
     }
 }
