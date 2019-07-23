24d106853b09f4fd80c69a87728e759c254ad199
duchengfu <duchengfu@163.com>
2019-07-23 16:33:59 +0800

    Interface VO

---
+++ src/main/java/com/keymanager/monitoring/vo/OptimizationMachineVO.java
@@ -0,0 +1,176 @@
+package com.keymanager.monitoring.vo;
+
+public class OptimizationMachineVO {
+    private int clearCookie;
+    private int disableStatistics;
+    private int disableVisitWebsite;
+
+    private int entryPageMinCount;
+    private int entryPageMaxCount;
+    private int pageRemainMinTime;
+    private int pageRemainMaxTime;
+    private int inputDelayMinTime;
+    private int inputDelayMaxTime;
+    private int slideDelayMinTime;
+    private int slideDelayMaxTime;
+    private int titleRemainMinTime;
+    private int titleRemainMaxTime;
+    private int optimizeKeywordCountPerIP;
+    private int randomlyClickNoResult;
+    private int maxUserCount;
+    private int page;
+    private int pageSize;
+    private String operationType;
+
+    public int getClearCookie() {
+        return clearCookie;
+    }
+
+    public void setClearCookie(int clearCookie) {
+        this.clearCookie = clearCookie;
+    }
+
+    public int getDisableStatistics() {
+        return disableStatistics;
+    }
+
+    public void setDisableStatistics(int disableStatistics) {
+        this.disableStatistics = disableStatistics;
+    }
+
+    public int getDisableVisitWebsite() {
+        return disableVisitWebsite;
+    }
+
+    public void setDisableVisitWebsite(int disableVisitWebsite) {
+        this.disableVisitWebsite = disableVisitWebsite;
+    }
+
+    public int getEntryPageMinCount() {
+        return entryPageMinCount;
+    }
+
+    public void setEntryPageMinCount(int entryPageMinCount) {
+        this.entryPageMinCount = entryPageMinCount;
+    }
+
+    public int getEntryPageMaxCount() {
+        return entryPageMaxCount;
+    }
+
+    public void setEntryPageMaxCount(int entryPageMaxCount) {
+        this.entryPageMaxCount = entryPageMaxCount;
+    }
+
+    public int getPageRemainMinTime() {
+        return pageRemainMinTime;
+    }
+
+    public void setPageRemainMinTime(int pageRemainMinTime) {
+        this.pageRemainMinTime = pageRemainMinTime;
+    }
+
+    public int getPageRemainMaxTime() {
+        return pageRemainMaxTime;
+    }
+
+    public void setPageRemainMaxTime(int pageRemainMaxTime) {
+        this.pageRemainMaxTime = pageRemainMaxTime;
+    }
+
+    public int getInputDelayMinTime() {
+        return inputDelayMinTime;
+    }
+
+    public void setInputDelayMinTime(int inputDelayMinTime) {
+        this.inputDelayMinTime = inputDelayMinTime;
+    }
+
+    public int getInputDelayMaxTime() {
+        return inputDelayMaxTime;
+    }
+
+    public void setInputDelayMaxTime(int inputDelayMaxTime) {
+        this.inputDelayMaxTime = inputDelayMaxTime;
+    }
+
+    public int getSlideDelayMinTime() {
+        return slideDelayMinTime;
+    }
+
+    public void setSlideDelayMinTime(int slideDelayMinTime) {
+        this.slideDelayMinTime = slideDelayMinTime;
+    }
+
+    public int getSlideDelayMaxTime() {
+        return slideDelayMaxTime;
+    }
+
+    public void setSlideDelayMaxTime(int slideDelayMaxTime) {
+        this.slideDelayMaxTime = slideDelayMaxTime;
+    }
+
+    public int getTitleRemainMinTime() {
+        return titleRemainMinTime;
+    }
+
+    public void setTitleRemainMinTime(int titleRemainMinTime) {
+        this.titleRemainMinTime = titleRemainMinTime;
+    }
+
+    public int getTitleRemainMaxTime() {
+        return titleRemainMaxTime;
+    }
+
+    public void setTitleRemainMaxTime(int titleRemainMaxTime) {
+        this.titleRemainMaxTime = titleRemainMaxTime;
+    }
+
+    public int getOptimizeKeywordCountPerIP() {
+        return optimizeKeywordCountPerIP;
+    }
+
+    public void setOptimizeKeywordCountPerIP(int optimizeKeywordCountPerIP) {
+        this.optimizeKeywordCountPerIP = optimizeKeywordCountPerIP;
+    }
+
+    public int getRandomlyClickNoResult() {
+        return randomlyClickNoResult;
+    }
+
+    public void setRandomlyClickNoResult(int randomlyClickNoResult) {
+        this.randomlyClickNoResult = randomlyClickNoResult;
+    }
+
+    public int getMaxUserCount() {
+        return maxUserCount;
+    }
+
+    public void setMaxUserCount(int maxUserCount) {
+        this.maxUserCount = maxUserCount;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(int pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public String getOperationType() {
+        return operationType;
+    }
+
+    public void setOperationType(String operationType) {
+        this.operationType = operationType;
+    }
+}
---
+++ src/main/java/com/keymanager/monitoring/vo/OptimizationVO.java
@@ -0,0 +1,22 @@
+package com.keymanager.monitoring.vo;
+
+public class OptimizationVO {
+    private OptimizationKeywordVO keywordVO;
+    private OptimizationMachineVO machineVO;
+
+    public OptimizationKeywordVO getKeywordVO() {
+        return keywordVO;
+    }
+
+    public void setKeywordVO(OptimizationKeywordVO keywordVO) {
+        this.keywordVO = keywordVO;
+    }
+
+    public OptimizationMachineVO getMachineVO() {
+        return machineVO;
+    }
+
+    public void setMachineVO(OptimizationMachineVO machineVO) {
+        this.machineVO = machineVO;
+    }
+}
