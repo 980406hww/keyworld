3f2a3de0dc2d15fe93ec981acb93b209cb4f3e22
steven.zhou <3047409257@qq.com>
2019-08-08 16:23:39 +0800

    开启无效关键字收录检查定时任务

--- src/main/db/modify_checingentered_keyword_interface/1. modify_checingentered_keyword_interface.sql
+++ src/main/db/modify_checingentered_keyword_interface/1. modify_checingentered_keyword_interface.sql
@@ -3,3 +3,10 @@ alter table t_customer_keyword change fEnteredKeywordRemarks fFailedCause VARCHA
 
 # 原fEnteredKeywordRemarks字段的值置空
 UPDATE t_customer_keyword SET fFailedCause = NULL WHERE fFailedCause > '';
+
+
+# 开启无效关键字检查定时任务
+UPDATE t_config SET fValue = 1 WHERE fConfigType = 'NoEnteredKeywordScheduleSwitch' AND fKey = 'SwitchNumber';
+
+# 如果不存在
+#INSERT INTO t_config VALUES('NoEnteredKeywordScheduleSwitch', 'SwitchNumber', 1);
