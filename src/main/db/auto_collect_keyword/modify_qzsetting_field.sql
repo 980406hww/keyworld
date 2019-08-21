d67cea4e690339dcfa787c5f82df07ef0fc62943
steven.zhou <3047409257@qq.com>
2019-08-21 10:11:45 +0800

    部分字段数据置为默认值的SQL

--- src/main/db/auto_collect_keyword/modify_qzsetting_field.sql
+++ src/main/db/auto_collect_keyword/modify_qzsetting_field.sql
@@ -1,3 +1,6 @@
 
 
 ALTER TABLE `db_keyword`.`t_qz_setting` MODIFY COLUMN `fAutoCrawlKeywordFlag` TINYINT DEFAULT 0 NULL COMMENT '是否自动运营 0：否、1是';
+
+# 字段数据置为默认值
+UPDATE t_qz_setting SET fAutoCrawlKeywordFlag = 0, fPcKeywordExceedMaxCount = 0, fPhoneKeywordExceedMaxCount = 0;
