5a8da7b9f1f7d56f2883fc3acb26dba49e2fc450
lhc0821 <1013217387@qq.com>
2019-08-27 19:04:43 +0800

    fix code

--- src/main/db/auto_test_data_statistics/2.add_resource_permission.sql
+++ src/main/db/auto_test_data_statistics/2.add_resource_permission.sql
@@ -1,8 +1,10 @@
-INSERT INTO t_resource ( fResourceName, fUrl, fOpenMode, fIconCls, fParentID, fSequence, fStatus, fOpened, fResourceType ,fCreateTime) VALUES
-    ( '查看测试数据统计', '/internal/algorithmAutoTest/showTestDataStatistics','ajax', '',
+
+# 添加查看测试数据统计资源
+INSERT INTO t_resource ( fResourceName, fUrl, fOpenMode, fIconCls, fParentID, fSequence, fStatus, fOpened, fResourceType ,fCreateTime)
+    VALUES( '查看测试数据统计', '/internal/algorithmAutoTest/showTestDataStatistics','ajax', '',
         (SELECT r.fUuid FROM t_resource r WHERE r.fUrl != '#' AND r.fResourceName = "测试计划管理"), 0, 0, 1, 1, NOW());
 
-# 补全权限给Technical角色
+# 分配查看测试数据统计权限给Technical角色
 INSERT INTO t_role_resource(fRoleID, fResourceId) SELECT tem_role.fUuid, tem_resource.fUuid FROM (
     (SELECT r.fUuid FROM t_role r WHERE r.fRoleName IN ('Technical')) tem_role,
     (SELECT tr.fUuid FROM t_resource tr WHERE tr.fResourceName IN ('查看测试数据统计')) tem_resource);
