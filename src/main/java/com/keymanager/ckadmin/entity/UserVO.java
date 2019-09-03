363e52760c7c7d1274085030b2000893bf57f5f0
yftpaper <yftpaper@gamil.com>
2019-09-03 14:18:30 +0800

    完成客户列表添加编辑按钮移植

---
+++ src/main/java/com/keymanager/ckadmin/entity/UserVO.java
@@ -0,0 +1,189 @@
+package com.keymanager.ckadmin.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.keymanager.monitoring.entity.Role;
+import com.keymanager.monitoring.entity.UserInfo;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @description：UserVo
+ * @author：zhixuan.wang
+ * @date：2015/10/1 14:51
+ */
+public class UserVO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long userUuid;
+
+    @NotBlank
+    @Length(min = 4, max = 64)
+    private String loginName;
+
+    private String userName;
+
+    @JsonIgnore
+    private String password;
+    @JsonIgnore
+    private String salt; // 密码加密盐
+
+    private Integer sex;
+
+    private Integer status;
+
+    private Integer organizationID;
+
+    private String phone;
+
+    private Date createTime;
+
+    private List<Role> rolesList;
+
+    private String organizationName;
+
+    private String roleIds;
+
+    private Date createdateStart;
+    private Date createdateEnd;
+
+    public Long getUserUuid() {
+        return userUuid;
+    }
+
+    public void setUserUuid(Long userUuid) {
+        this.userUuid = userUuid;
+    }
+
+    public String getLoginName() {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName) {
+        this.loginName = loginName == null ? null : loginName.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Integer getOrganizationID() {
+        return organizationID;
+    }
+
+    public void setOrganizationID(Integer organizationID) {
+        this.organizationID = organizationID;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public List<Role> getRolesList() {
+        return rolesList;
+    }
+
+    public void setRolesList(List<Role> rolesList) {
+        this.rolesList = rolesList;
+    }
+
+    public String getOrganizationName() {
+        return organizationName;
+    }
+
+    public void setOrganizationName(String organizationName) {
+        this.organizationName = organizationName;
+    }
+
+    public String getRoleIds() {
+        return roleIds;
+    }
+
+    public void setRoleIds(String roleIds) {
+        this.roleIds = roleIds;
+    }
+
+    public Date getCreatedateStart() {
+        return createdateStart;
+    }
+
+    public void setCreatedateStart(Date createdateStart) {
+        this.createdateStart = createdateStart;
+    }
+
+    public Date getCreatedateEnd() {
+        return createdateEnd;
+    }
+
+    public void setCreatedateEnd(Date createdateEnd) {
+        this.createdateEnd = createdateEnd;
+    }
+
+    /**
+     * 比较vo和数据库中的用户是否同一个user，采用id比较
+     *
+     * @param user 用户
+     * @return 是否同一个人
+     */
+    public boolean equalsUser(UserInfo user) {
+        if (user == null) {
+            return false;
+        }
+        String userId = user.getLoginName();
+        if (userUuid == null || userId == null) {
+            return false;
+        }
+        return userUuid.equals(userId);
+    }
+}
--- src/main/java/com/keymanager/ckadmin/service/impl/CustomerService2.java
+++ src/main/java/com/keymanager/ckadmin/service/impl/CustomerService2.java
@@ -8,12 +8,11 @@ import com.keymanager.ckadmin.dao.CustomerDao2;
 import com.keymanager.ckadmin.entity.Customer;
 import com.keymanager.ckadmin.service.CustomerInterface;
 import com.keymanager.ckadmin.service.CustomerKeywordInterface;
+
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import javax.annotation.Resource;
+
 import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
@@ -44,33 +43,33 @@ public class CustomerService2 extends ServiceImpl<CustomerDao2, Customer> implem
 
     @Override
     public Page<Customer> searchCustomers(Page<Customer> page, CustomerCriteria customerCriteria) {
-        List<Customer> customerList = customerDao2.searchCustomers(page,customerCriteria);
-        if(CollectionUtils.isNotEmpty(customerList)){
+        List<Customer> customerList = customerDao2.searchCustomers(page, customerCriteria);
+        if (CollectionUtils.isNotEmpty(customerList)) {
             List<Long> customerUuids = new ArrayList<Long>();
-            for(Customer customer : customerList){
+            for (Customer customer : customerList) {
                 customerUuids.add(customer.getUuid());
             }
             List<Map> customerKeywordCountMap = customerKeywordService2.getCustomerKeywordsCount(customerUuids, customerCriteria.getTerminalType(),customerCriteria.getEntryType());
             Map<Integer, Map> customerUuidKeywordCountMap = new HashMap<Integer, Map>();
-            for(Map map : customerKeywordCountMap){
-                customerUuidKeywordCountMap.put((Integer)map.get("customerUuid"), map);
+            for (Map map : customerKeywordCountMap) {
+                customerUuidKeywordCountMap.put((Integer) map.get("customerUuid"), map);
             }
-            for(Customer customer : customerList){
-                if(!customer.getLoginName().equals(customerCriteria.getLoginName())){
+            for (Customer customer : customerList) {
+                if (!customer.getLoginName().equals(customerCriteria.getLoginName())) {
                     customer.setSaleRemark(null);
                     customer.setTelphone(null);
                     customer.setQq(null);
                     customer.setEmail(null);
                 }
                 Map map = customerUuidKeywordCountMap.get(customer.getUuid().intValue());
-                if(map != null) {
-                    Long totalCount = (Long)map.get("totalCount");
-                    if(totalCount != null) {
+                if (map != null) {
+                    Long totalCount = (Long) map.get("totalCount");
+                    if (totalCount != null) {
                         customer.setKeywordCount(totalCount.intValue());
                     }
 
-                    BigDecimal activeCount = (BigDecimal)map.get("activeCount");
-                    if(activeCount != null) {
+                    BigDecimal activeCount = (BigDecimal) map.get("activeCount");
+                    if (activeCount != null) {
                         customer.setActiveKeywordCount(activeCount.intValue());
                     }
                 }
@@ -81,8 +80,43 @@ public class CustomerService2 extends ServiceImpl<CustomerDao2, Customer> implem
     }
 
     @Override
+    public void saveCustomer(Customer customer, String loginName) {
+        //修改
+        if (null != customer.getUuid()) {
+            updateCustomer(customer, loginName);
+        } else {//添加
+            customer.setUpdateTime(new Date());
+            customer.setLoginName(loginName);
+            customerDao2.insert(customer);
+        }
+    }
+
+    @Override
     public void deleteCustomer(long uuid) {
         customerDao2.deleteById(uuid);
         customerKeywordService2.deleteCustomerKeywords(uuid);
+    }
+
+    private void updateCustomer(Customer customer, String loginName) {
+        Customer oldCustomer = customerDao2.selectById(customer.getUuid());
+        if (oldCustomer != null) {
+            if (oldCustomer.getLoginName().equals(loginName)) {
+                oldCustomer.setQq(customer.getQq());
+                oldCustomer.setEmail(customer.getEmail());
+                oldCustomer.setTelphone(customer.getTelphone());
+                oldCustomer.setSaleRemark(customer.getSaleRemark());
+            }
+            oldCustomer.setContactPerson(customer.getContactPerson());
+            oldCustomer.setAlipay(customer.getAlipay());
+            oldCustomer.setPaidFee(customer.getPaidFee());
+            oldCustomer.setRemark(customer.getRemark());
+            oldCustomer.setType(customer.getType());
+            oldCustomer.setStatus(customer.getStatus());
+            oldCustomer.setDailyReportIdentify(customer.getDailyReportIdentify());
+            oldCustomer.setLoginName(customer.getLoginName());
+            oldCustomer.setEntryType(customer.getEntryType());
+            oldCustomer.setUpdateTime(new Date());
+            customerDao2.updateById(oldCustomer);
+        }
     }
 }
