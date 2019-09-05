09abedbb6f714b8dda0aa6c960c1196a225f57c5
yftpaper <yftpaper@gamil.com>
2019-09-05 17:16:07 +0800

    更新t_customer数据库字段

--- src/main/java/com/keymanager/ckadmin/entity/Customer.java
+++ src/main/java/com/keymanager/ckadmin/entity/Customer.java
@@ -17,11 +17,11 @@ public class Customer extends BaseEntity {
     @TableField(value = "fExternalAccount")
     private String externalAccount;
 
-    @TableField(value = "fSearchEngine")
-    private String searchEngine;
+//    @TableField(value = "fSearchEngine")
+//    private String searchEngine;
 
-    @TableField(value = "fEntryType")
-    private String entryType;
+//    @TableField(value = "fEntryType")
+//    private String entryType;
 
     @NotNull
     @TableField(value = "fContactPerson")
@@ -53,14 +53,14 @@ public class Customer extends BaseEntity {
     @TableField(value = "fDailyReportIdentify")
     private boolean dailyReportIdentify;
 
-    @TableField(value = "fActiveHour")
-    private String activeHour;
+//    @TableField(value = "fActiveHour")
+//    private String activeHour;
 
-    @TableField(value = "fInActiveHour")
-    private String inActiveHour;
+//    @TableField(value = "fInActiveHour")
+//    private String inActiveHour;
 
-    @TableField(value = "fType")
-    private String type;
+//    @TableField(value = "fType")
+//    private String type;
 
     @TableField(exist = false)
     private int keywordCount;
@@ -90,21 +90,21 @@ public class Customer extends BaseEntity {
         this.externalAccount = externalAccount;
     }
 
-    public String getSearchEngine() {
-        return searchEngine;
-    }
+//    public String getSearchEngine() {
+//        return searchEngine;
+//    }
+//
+//    public void setSearchEngine(String searchEngine) {
+//        this.searchEngine = searchEngine;
+//    }
 
-    public void setSearchEngine(String searchEngine) {
-        this.searchEngine = searchEngine;
-    }
-
-    public String getEntryType() {
-        return entryType;
-    }
-
-    public void setEntryType(String entryType) {
-        this.entryType = entryType;
-    }
+//    public String getEntryType() {
+//        return entryType;
+//    }
+//
+//    public void setEntryType(String entryType) {
+//        this.entryType = entryType;
+//    }
 
     public String getContactPerson() {
         return contactPerson;
@@ -170,13 +170,13 @@ public class Customer extends BaseEntity {
         this.remark = remark;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+//    public String getType() {
+//        return type;
+//    }
+//
+//    public void setType(String type) {
+//        this.type = type;
+//    }
 
     public int getKeywordCount() {
         return keywordCount;
@@ -202,21 +202,21 @@ public class Customer extends BaseEntity {
         this.activeKeywordCount = activeKeywordCount;
     }
 
-    public String getActiveHour() {
-        return activeHour;
-    }
-
-    public void setActiveHour(String activeHour) {
-        this.activeHour = activeHour;
-    }
-
-    public String getInActiveHour() {
-        return inActiveHour;
-    }
-
-    public void setInActiveHour(String inActiveHour) {
-        this.inActiveHour = inActiveHour;
-    }
+//    public String getActiveHour() {
+//        return activeHour;
+//    }
+//
+//    public void setActiveHour(String activeHour) {
+//        this.activeHour = activeHour;
+//    }
+//
+//    public String getInActiveHour() {
+//        return inActiveHour;
+//    }
+//
+//    public void setInActiveHour(String inActiveHour) {
+//        this.inActiveHour = inActiveHour;
+//    }
 
     public String getUpdateInterval() {
         return updateInterval;
