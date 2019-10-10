e12dc6662292be8debd1e85f080b22b44a4ddbae
lhc0821 <1013217387@qq.com>
2019-10-10 18:43:08 +0800

    分页数据展示

--- src/main/java/com/keymanager/ckadmin/controller/internal/rest/GroupSettingController.java
+++ src/main/java/com/keymanager/ckadmin/controller/internal/rest/GroupSettingController.java
@@ -1,6 +1,26 @@
 package com.keymanager.ckadmin.controller.internal.rest;
 
+import com.baomidou.mybatisplus.plugins.Page;
+import com.keymanager.ckadmin.common.result.ResultBean;
+import com.keymanager.ckadmin.criteria.GroupSettingCriteria;
+import com.keymanager.ckadmin.entity.OperationCombine;
+import com.keymanager.ckadmin.service.GroupSettingService;
+import com.keymanager.ckadmin.util.ReflectUtils;
+import com.keymanager.ckadmin.util.SQLFilterUtils;
+import com.keymanager.monitoring.common.shiro.ShiroUser;
+import com.keymanager.monitoring.controller.rest.internal.GroupSettingRestController;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.Resource;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,11 +36,42 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping(value = "/internal/groupsetting")
 public class GroupSettingController {
 
+    private static Logger logger = LoggerFactory.getLogger(GroupSettingController.class);
+    @Resource(name = "groupSettingService2")
+    private GroupSettingService groupSettingService;
+
+    @RequiresPermissions("/internal/groupsetting/searchGroupSettings")
     @GetMapping("/toGroupSettings")
-    public ModelAndView toGroupSettings(){
+    public ModelAndView toGroupSettings() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("groupSettings/groupSetting");
         return mv;
+    }
+
+    @RequiresPermissions("/internal/groupsetting/searchGroupSettings")
+    @PostMapping(value = "/getGroupSettings")
+    public ResultBean getGroupSettings(@RequestBody GroupSettingCriteria groupSettingCriteria) {
+        ResultBean resultBean = new ResultBean();
+        if (SQLFilterUtils.sqlInject(groupSettingCriteria.toString())) {
+            resultBean.setCode(400);
+            resultBean.setMsg("查询参数错误或包含非法字符，请检查后重试！");
+            return resultBean;
+        }
+        try {
+            Page<OperationCombine> page = new Page<>(groupSettingCriteria.getPage(), groupSettingCriteria.getLimit());
+            page = groupSettingService.searchGroupSettings(page, groupSettingCriteria);
+            List<OperationCombine> operationCombines = page.getRecords();
+            resultBean.setCode(200);
+            resultBean.setCount(page.getTotal());
+            resultBean.setMsg("");
+            resultBean.setData(operationCombines);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            resultBean.setCode(400);
+            resultBean.setMsg("未知错误");
+            return resultBean;
+        }
+        return resultBean;
     }
 
 }
--- src/main/webapp/static/ok-admin/page/groupSetting/groupSetting.js
+++ src/main/webapp/static/ok-admin/page/groupSetting/groupSetting.js
@@ -43,7 +43,7 @@ layui.use(['element', 'form', 'jquery', 'laypage', 'okLayer', 'layer'], function
             pageConf.limit = 30;
         }
         $.ajax({
-            url: '/internal/customer/getCustomers',
+            url: '/internal/groupsetting/getGroupSettings',
             data: JSON.stringify(pageConf),
             headers: {
                 'Accept': 'application/json',
@@ -70,12 +70,7 @@ layui.use(['element', 'form', 'jquery', 'laypage', 'okLayer', 'layer'], function
                     }
                 });
                 init_data(result.data);
-                let entryType = $('#entryType').val();
-                let postData = {};
-                postData.entryType = entryType;
-                init_customerTypeCount(postData);
-                form.render();
-                // layer.msg('加载完成', {icon: 6});
+
             },
             error: function () {
                 layer.msg('获取用户失败，请稍后再试', {icon: 5});
@@ -84,63 +79,192 @@ layui.use(['element', 'form', 'jquery', 'laypage', 'okLayer', 'layer'], function
     }
     function init_data(data) {
         $("#data_list").html('');
-        $.each(data, function (index, obj) {
-            let item = '<div class="data-item">\n'
+        $.each(data, function (index, item) {
+            console.log(item.groupSettings)
+            let htm = '';
+            htm += '<div class="data-item">\n'
+                +'<input type="hidden" name="operationCombineUuid" value="'+item.uuid+'}">'
                 + '                        <div class="layui-row">\n'
                 + '                            <div class="layui-col-md12">\n'
                 + '                                <div class="data-head">\n'
-                + '                                <div class="layui-col-md1">操作组合名称</div>\n'
-                + '                                <div class="layui-col-md1">创建人</div>\n'
-                + '                                <div class="layui-col-md1">最大无效点击数</div>\n'
-                + '                                <div class="layui-col-md6">分组:pc_pm_xzhuang,pc_pm_yilufa,pc_pm_zhuoguang,qzw_xl,qzw_zo,wjianwu_out,wjianwu_check</div>\n'
-                + '                                <div class="layui-col-md3" title="操作">\n'
-                + '                                    <div class="layui-col-md2">分组详情</div>\n'
-                + '                                    <div class="layui-col-md3">新增操作组设置</div>\n'
-                + '                                    <div class="layui-col-md4">批量修改操作组设置</div>\n'
-                + '                                    <div class="layui-col-md3">删除操作组合</div>\n'
-                + '                                </div>\n'
-                + '                            </div>\n'
-                + '                            <div class="layui-col-md12">\n'
-                + '                                <div class="data-body">\n'
-                + '                                    <div class="body-title">\n'
-                + '                                            <div class="layui-col-md1" style="width: 10%">操作类型</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 10%">分组设置占比</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 8%">网站统计</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 8%">目标网站</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 7%">页数</div>\n'
-                + '\n'
-                + '                                            <div class="layui-col-md1"style="width: 7%">每页条数</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 10%">Cookie设置</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 10%">进入页次数</div>\n'
-                + '                                            <div class="layui-col-md2"style="width: 10%">刷多少个词换IP</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 10%">没结果随机点</div>\n'
-                + '                                            <div class="layui-col-md1"style="width: 10%">操作</div>\n'
-                + '\n'
-                + '                                    </div>\n'
-                + '                                    <div class="body-content">\n'
-                + '                                        <div class="layui-col-md1" style="width: 10%">pc_pm_1</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 10%">50%</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 8%">开放</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 8%">访问</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 7%">5</div>\n'
-                + '\n'
-                + '                                        <div class="layui-col-md1"style="width: 7%">10</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 10%">不清理Cookie</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 10%">0 - 0</div>\n'
-                + '                                        <div class="layui-col-md2"style="width: 10%">1</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 10%">是</div>\n'
-                + '                                        <div class="layui-col-md1"style="width: 10%">修改 | 删除</div>\n'
-                + '\n'
-                + '                                    </div>\n'
-                + '                                </div>\n'
-                + '\n'
-                + '                            </div>\n'
-                + '\n'
-                + '                        </div>\n'
-                + '                    </div>\n'
-                + '                </div>';
-            $("#data_list").append(item)
+                + '                                     <div class="layui-col-md1 skip">'+item.operationCombineName+'</div>\n'
+                + '                                     <div class="layui-col-md1">'+item.creator+'</div>\n'
+                + '                                     <div class="layui-col-md1">最大无效点击数:'+item.maxInvalidCount+'</div>\n'
+                + '                                     <div class="layui-col-md6">分组:'+getGroupNameStrByUuid(item.uuid)+'</div>\n'
+                + '                                     <div class="layui-col-md3 operation" title="操作">\n'
+                + '                                         <div class="layui-col-md2">'
+                + '                                              <a href="javascript:void(0)" onclick=showGroupQueueDialog("' + item.uuid + '")>分组详情 </a>'
+                + '                                          </div>\n'
+                + '                                         <div class="layui-col-md3">'
+                + '                                              <a href="javascript:void(0)" onclick=showGroupSettingDialog("' + item.uuid + '")>新增操作组设置 </a>'
+                + '                                         </div>\n'
+                + '                                         <div class="layui-col-md4">'
+                + '                                              <a href="javascript:void(0)" onclick=showUpdateGroupDialog("' + item.uuid + '")>批量修改操作组设置 </a>'
+                + '                                         </div>\n'
+                + '                                         <div class="layui-col-md3">'
+                + '                                              <a href="javascript:void(0)" onclick=delOperationCombine("' + item.uuid + '")>删除操作组合 </a>'
+                + '                                         </div>\n'
+                + '                                     </div>\n'
+                + '                            </div>'
+                + '                           </div>';
+            if(item.groupSettings.length > 0){
+                htm += '                            <div class="layui-col-md12">\n'
+                    + '                                <div class="data-body">\n'
+                    + '                                    <div class="body-title">\n'
+                    + '                                            <div class="layui-col-md1" style="width: 10%">操作类型</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 10%">分组设置占比</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 8%">网站统计</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 8%">目标网站</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 7%">页数</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 7%">每页条数</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 10%">Cookie设置</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 10%">进入页次数</div>\n'
+                    + '                                            <div class="layui-col-md2"style="width: 10%">刷多少个词换IP</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 10%">没结果随机点</div>\n'
+                    + '                                            <div class="layui-col-md1"style="width: 10%">操作</div>\n'
+                    + '                                    </div>\n';
+                $.each(item.groupSettings, function (index2, groupSetting) {
+                    htm +='                                    <div class="body-content">\n'
+                        + '                                         <div class="layui-col-md1" style="width: 10%">'
+                        +                                               groupSetting.operationType
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 10%">'
+                        +                                               groupSetting.machineUsedPercent+'%'
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 8%">'
+                        +                                               getStatisticText(groupSetting.disableStatistics)
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 8%">'
+                        +                                               getVisitWebsiteText(groupSetting.disableVisitWebsite)
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 7%">'
+                        +                                               groupSetting.page
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 7%">'
+                        +                                               getPageSizeText(groupSetting.pageSize)
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 10%">'
+                        +                                               getCookieText(groupSetting.clearCookie)
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md1"style="width: 10%">'
+                        +                                               groupSetting.entryPageMinCount + ' - ' + groupSetting.entryPageMaxCount
+                        + '                                         </div>\n'
+                        + '                                         <div class="layui-col-md2"style="width: 10%">'
+                        +                                               groupSetting.optimizeKeywordCountPerIP
+                        + '                                        </div>\n'
+                        + '                                        <div class="layui-col-md1"style="width: 10%">'
+                        +                                               getRandomlyClickNoResultText(groupSetting.randomlyClickNoResult)
+                        + '                                        </div>\n'
+                        + '                                        <div class="layui-col-md1"style="width: 10%">'
+                        + '                                             <a href="javascript:void(0)" onclick=showGroupSettingDialog("' + groupSetting.uuid +'")>修改 </a>'
+                        + '                                             <a href="javascript:void(0)" onclick=delGroupSetting("' + groupSetting.uuid + '")>删除 </a>'
+                        + '                                         </div>'
+                        + '                                    </div>\n';
+                });
+                htm +='                                </div>\n'
+                    + '                            </div>\n'
+                    + '                        </div>\n'
+                    + '                    </div>\n'
+                    + '                </div>';
+            }
+
+
+            $("#data_list").append(htm)
+            $("#data_list").trigger("create");
         })
     }
 
+    window.getStatisticText  = function(disableStatistics){
+        let htm = '';
+        switch (disableStatistics) {
+            case 0:
+                htm = '开放';
+                break;
+           default:
+                htm = '关闭';
+
+        }
+        return htm;
+    };
+
+    window.getVisitWebsiteText  = function(disableVisitWebsite){
+        let htm = '';
+        switch (disableVisitWebsite) {
+            case 0:
+                htm = '访问';
+                break;
+            default:
+                htm = '不访问';
+        }
+        return htm;
+    };
+
+    window.getPageSizeText  = function(pageSize){
+        let htm = '';
+        switch (pageSize) {
+            case 0:
+                htm = '10';
+                break;
+            case 1:
+                htm = '20';
+                break;
+            default:
+                htm = '50';
+        }
+        return htm;
+    };
+
+    window.getCookieText  = function(clearCookie){
+        let htm = '';
+        switch (clearCookie) {
+            case 0:
+                htm = '不清理Cookie';
+                break;
+            case 1:
+                htm = '每次都清理Cookie';
+                break;
+            case 2:
+                htm = '随机操作清理Cookie';
+                break;
+            default:
+                htm = 'N次操作清理Cookie';
+        }
+        return htm;
+    };
+
+    window.getRandomlyClickNoResultText  = function(randomlyClickNoResult){
+        let htm = '';
+        switch (randomlyClickNoResult) {
+            case 1:
+                htm = '是';
+                break;
+            default:
+                htm = '否';
+        }
+        return htm;
+    };
+
+    window.getGroupNameStrByUuid = function (uuid) {
+        return uuid+'分组分组分组'
+    };
+
+    window.showGroupQueueDialog = function (uuid) {
+
+    };
+
+    window.showGroupSettingDialog = function (uuid) {
+
+    };
+
+    window.showUpdateGroupDialog = function (uuid) {
+
+    };
+
+    window.delOperationCombine = function (uuid) {
+
+    };
+
+    window.delGroupSetting = function (uuid) {
+
+    };
 });
