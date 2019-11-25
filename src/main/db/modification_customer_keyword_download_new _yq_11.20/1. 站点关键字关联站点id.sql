19a3e7ba2118d0a103e04d306a47b3dedfedb231
zhoukai <3047409257@qq.com>
2019-11-25 15:03:54 +0800

    客户关键词关联站点id sql修改

--- src/main/db/modification_customer_keyword_download_new _yq_11.20/1. 站点关键字关联站点id.sql
+++ src/main/db/modification_customer_keyword_download_new _yq_11.20/1. 站点关键字关联站点id.sql
@@ -1,106 +1,138 @@
 
-# 下面的处理并未完全处理好全站关键词关联站点id，前台功能辅助下可实现完全关联
-# 给全站未下架电脑端关键字关联对应站点uuid
+# 设置全站关键词fqzsettinguuid为null
+UPDATE t_customer_keyword SET fQZSettingUuid = NULL;
+
+# 给全站pc端关键字关联对应站点uuid
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `pro_ck_qz_pc_migration`;
-CREATE PROCEDURE pro_ck_qz_pc_migration ()
+CREATE PROCEDURE pro_ck_qz_pc_migration ( )
 BEGIN
-	DECLARE ck_customeruuid INT ( 11 ) DEFAULT NULL;
-	DECLARE ck_url VARCHAR ( 128 ) DEFAULT NULL;
-	DECLARE ck_searchengine VARCHAR ( 100 ) DEFAULT NULL;
-	DECLARE ck_groupname VARCHAR ( 20 ) DEFAULT NULL;
+	DECLARE qz_uuid INT ( 11 ) DEFAULT NULL;
+	DECLARE c_uuid INT ( 11 ) DEFAULT NULL;
+	DECLARE qz_search_engine VARCHAR ( 20 ) DEFAULT NULL;
 	DECLARE done INT DEFAULT 0;
-	DECLARE ck_qz_pc_migration_cursor CURSOR FOR (
-			SELECT ck.fCustomerUuid, ck.fUrl, ck.fSearchEngine, ck.fOptimizeGroupName
-			FROM t_customer_keyword ck
-			LEFT JOIN t_qz_setting qs
-			ON qs.fCustomerUuid = ck.fCustomerUuid
-			WHERE ck.fTerminalType = 'PC'
-			AND ck.fType = 'qz'
-			AND ck.fQZSettingUuid IS NULL
-			AND qs.fSearchEngine = ck.fSearchEngine
-			AND qs.fPcGroup > ''
-			AND qs.fPcGroup = ck.fOptimizeGroupName
-			GROUP BY ck.fCustomerUuid, ck.fUrl, ck.fSearchEngine, ck.fOptimizeGroupName
-	);
+	DECLARE ck_qz_pc_cursor CURSOR FOR (
+		SELECT
+			qs.fUuid,
+			qs.fCustomerUuid,
+			qs.fSearchEngine
+		FROM t_qz_setting qs
+		WHERE qs.fPcGroup > ''
+			AND EXISTS (
+				SELECT 1
+				FROM t_customer_keyword ck
+				LEFT JOIN t_customer c ON ck.fCustomerUuid = c.fUuid
+				LEFT JOIN t_userinfo ui ON ui.fLoginName = c.fUserID
+				WHERE qs.fCustomerUuid = ck.fCustomerUuid
+				AND ck.fType = 'qz'
+				AND ck.fTerminalType = 'PC'
+				AND ui.fStatus = 0
+				LIMIT 1
+			)
+		GROUP BY qs.fCustomerUuid, qs.fSearchEngine
+		HAVING COUNT(qs.fCustomerUuid) = 1
+		ORDER BY qs.fCustomerUuid ASC
+		);
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	START TRANSACTION;
-	OPEN ck_qz_pc_migration_cursor;
+	OPEN ck_qz_pc_cursor;
 	out_loop:
 	LOOP
-		FETCH ck_qz_pc_migration_cursor INTO ck_customeruuid, ck_url, ck_searchengine, ck_groupname;
+		FETCH ck_qz_pc_cursor INTO qz_uuid, c_uuid, qz_search_engine;
 		IF done = 1 THEN
-		LEAVE out_loop;
+			LEAVE out_loop;
 		END IF;
 		BEGIN
-			SET @qzSettingUuid = (
-					SELECT fUuid FROM t_qz_setting
-					WHERE fCustomerUuid = ck_customeruuid AND fDomain = ck_url AND fSearchEngine = ck_searchengine AND fPcGroup = ck_groupname
-			);
-			IF @qzSettingUuid > '' THEN
-					UPDATE t_customer_keyword SET fQZSettingUuid = @qzSettingUuid
-					WHERE fCustomerUuid = ck_customeruuid AND fUrl = ck_url AND fTerminalType = 'PC' AND fSearchEngine = ck_searchengine
-					AND fOptimizeGroupName = ck_groupname AND fQZSettingUuid IS NULL;
-			END IF;
+				UPDATE t_customer_keyword SET fQZSettingUuid = qz_uuid WHERE fCustomerUuid = c_uuid AND fSearchEngine = qz_search_engine
+				AND fTerminalType = 'PC' AND fType = 'qz';
 		END;
 		SET done = 0;
 	END LOOP out_loop;
-	CLOSE ck_qz_pc_migration_cursor;
+	CLOSE ck_qz_pc_cursor;
 	COMMIT;
 END;
 
 CALL pro_ck_qz_pc_migration();
-
 DROP PROCEDURE pro_ck_qz_pc_migration;
 
-# 给全站未下架移动端关键字关联对应站点uuid
+# 给全站phone端关键字关联对应站点uuid
 DELIMITER $$
 DROP PROCEDURE IF EXISTS `pro_ck_qz_phone_migration`;
-CREATE PROCEDURE pro_ck_qz_phone_migration ()
+CREATE PROCEDURE pro_ck_qz_phone_migration ( )
 BEGIN
-	DECLARE ck_customeruuid INT ( 11 ) DEFAULT NULL;
-	DECLARE ck_url VARCHAR ( 128 ) DEFAULT NULL;
-	DECLARE ck_searchengine VARCHAR ( 100 ) DEFAULT NULL;
-	DECLARE ck_groupname VARCHAR ( 20 ) DEFAULT NULL;
+	DECLARE qz_uuid INT ( 11 ) DEFAULT NULL;
+	DECLARE c_uuid INT ( 11 ) DEFAULT NULL;
+	DECLARE qz_search_engine VARCHAR ( 20 ) DEFAULT NULL;
 	DECLARE done INT DEFAULT 0;
-	DECLARE ck_qz_phone_migration_cursor CURSOR FOR (
-			SELECT ck.fCustomerUuid, ck.fUrl, ck.fSearchEngine, ck.fOptimizeGroupName
-			FROM t_customer_keyword ck
-			LEFT JOIN t_qz_setting qs
-			ON qs.fCustomerUuid = ck.fCustomerUuid
-			WHERE ck.fTerminalType = 'Phone'
-			AND ck.fType = 'qz'
-			AND ck.fQZSettingUuid IS NULL
-			AND qs.fSearchEngine = ck.fSearchEngine
-			AND qs.fPhoneGroup > ''
-			AND qs.fPhoneGroup = ck.fOptimizeGroupName
-			GROUP BY ck.fCustomerUuid, ck.fUrl, ck.fSearchEngine, ck.fOptimizeGroupName
-	);
+	DECLARE ck_qz_phone_cursor CURSOR FOR (
+		SELECT
+			qs.fUuid,
+			qs.fCustomerUuid,
+			qs.fSearchEngine
+		FROM t_qz_setting qs
+		WHERE qs.fPhoneGroup > ''
+			AND EXISTS (
+				SELECT 1
+				FROM t_customer_keyword ck
+				LEFT JOIN t_customer c ON ck.fCustomerUuid = c.fUuid
+				LEFT JOIN t_userinfo ui ON ui.fLoginName = c.fUserID
+				WHERE qs.fCustomerUuid = ck.fCustomerUuid
+				AND ck.fType = 'qz'
+				AND ck.fTerminalType = 'Phone'
+				AND ui.fStatus = 0
+				LIMIT 1
+			)
+		GROUP BY qs.fCustomerUuid, qs.fSearchEngine
+		HAVING COUNT(qs.fCustomerUuid) = 1
+		ORDER BY qs.fCustomerUuid ASC
+		);
 	DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
 	START TRANSACTION;
-	OPEN ck_qz_phone_migration_cursor;
+	OPEN ck_qz_phone_cursor;
 	out_loop:
 	LOOP
-		FETCH ck_qz_phone_migration_cursor INTO ck_customeruuid, ck_url, ck_searchengine, ck_groupname;
+		FETCH ck_qz_phone_cursor INTO qz_uuid, c_uuid, qz_search_engine;
 		IF done = 1 THEN
-		LEAVE out_loop;
+			LEAVE out_loop;
 		END IF;
 		BEGIN
-			SET @qzSettingUuid = (
-					SELECT fUuid FROM t_qz_setting
-					WHERE fCustomerUuid = ck_customeruuid AND fDomain = ck_url AND fSearchEngine = ck_searchengine AND fPhoneGroup = ck_groupname
-			);
-			IF @qzSettingUuid > '' THEN
-					UPDATE t_customer_keyword SET fQZSettingUuid = @qzSettingUuid
-					WHERE fCustomerUuid = ck_customeruuid AND fUrl = ck_url AND fTerminalType = 'Phone' AND fSearchEngine = ck_searchengine
-					AND fOptimizeGroupName = ck_groupname AND fQZSettingUuid IS NULL;
-			END IF;
+				UPDATE t_customer_keyword SET fQZSettingUuid = qz_uuid WHERE fCustomerUuid = c_uuid AND fSearchEngine = qz_search_engine
+				AND fTerminalType = 'Phone' AND fType = 'qz';
 		END;
 		SET done = 0;
 	END LOOP out_loop;
-	CLOSE ck_qz_phone_migration_cursor;
+	CLOSE ck_qz_phone_cursor;
 	COMMIT;
 END;
-CALL pro_ck_qz_phone_migration();
 
+CALL pro_ck_qz_phone_migration();
 DROP PROCEDURE pro_ck_qz_phone_migration;
+
+# 搜索客户-站点 一对多的数据 导出excel给到运营处理
+SELECT c.fContactPerson, t1.fDomain, t1.fSearchEngine, t1.fPcGroup, t1.fPhoneGroup
+FROM t_qz_setting t1
+LEFT JOIN t_customer c
+ON t1.fCustomerUuid = c.fUuid
+WHERE t1.fCustomerUuid IN (
+	SELECT qs.fCustomerUuid
+	FROM t_qz_setting qs
+	WHERE qs.fPcGroup > ''
+	AND EXISTS (
+			SELECT 1
+			FROM t_customer_keyword ck
+			LEFT JOIN t_customer c
+			ON ck.fCustomerUuid = c.fUuid
+			LEFT JOIN t_userinfo ui
+			ON ui.fLoginName = c.fUserID
+			WHERE qs.fCustomerUuid = ck.fCustomerUuid
+			AND ck.fType = 'qz'
+			AND ck.fTerminalType = 'PC'
+			AND ui.fStatus = 0
+			LIMIT 1
+	)
+	GROUP BY qs.fCustomerUuid, qs.fSearchEngine
+	HAVING COUNT(qs.fCustomerUuid) > 1
+	ORDER BY qs.fCustomerUuid ASC
+)
+ORDER BY c.fContactPerson;
+
