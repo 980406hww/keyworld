2f5e28476efa206f015d81ef79b4213a414e1875
zhoukai <3047409257@qq.com>
2019-12-10 18:35:56 +0800

    处理站点关键字搜索引擎为空的问题

---
+++ src/main/db/all_2019_12_10/1. 处理站点关键字搜索引擎.sql
@@ -0,0 +1,6 @@
+﻿
+# 服务端端口修改时，新增一部分站点并加了词，从而出现一些关键词没有搜索引擎
+UPDATE t_customer_keyword ck, t_qz_setting qs
+SET ck.fSearchEngine = qs.fSearchEngine
+WHERE ck.fQZSettingUuid = qs.fUuid
+AND ck.fCustomerUuid = qs.fCustomerUuid;
