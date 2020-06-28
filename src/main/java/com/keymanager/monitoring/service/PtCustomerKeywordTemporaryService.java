8a9362dd7e0117025492ec04f5f24e3ecb705648
zhoukai <3047409257@qq.com>
2020-06-28 14:03:27 +0800

    控制每次同步的数据量为20000

---
+++ src/main/java/com/keymanager/monitoring/service/PtCustomerKeywordTemporaryService.java
@@ -0,0 +1,33 @@
+package com.keymanager.monitoring.service;
+
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import com.keymanager.monitoring.dao.PtCustomerKeywordTemporaryDao;
+import com.keymanager.monitoring.entity.PtCustomerKeywordTemporary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PtCustomerKeywordTemporaryService extends ServiceImpl<PtCustomerKeywordTemporaryDao, PtCustomerKeywordTemporary> {
+
+    @Autowired
+    private PtCustomerKeywordTemporaryDao ptCustomerKeywordTemporaryDao;
+
+
+    public void migrationRecordToPtCustomerKeyword(Long uuid, String pt) {
+        ptCustomerKeywordTemporaryDao.migrationRecordToPtCustomerKeyword(uuid, pt);
+    }
+
+    public void cleanPtCustomerKeyword() {
+        ptCustomerKeywordTemporaryDao.cleanPtCustomerKeyword();
+    }
+
+    public int searchPtKeywordTemporaryCount() {
+        return ptCustomerKeywordTemporaryDao.searchPtKeywordTemporaryCount();
+    }
+
+    public void updatePtKeywordMarks() {
+        ptCustomerKeywordTemporaryDao.updatePtKeywordMarks();
+    }
+}
